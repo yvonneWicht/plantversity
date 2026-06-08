cf3eddf6d52a9528ec51fcfdde1e6c195628714d
Yvonne Wicht <yvonne.wicht@iu-study.org>
2026-06-08 19:29:13 +0200

    initialize project

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4 text-center bg-green-100 w-screen h-screen">
+    <NuxtRouteAnnouncer/>
+    <h1 class="text-xl font-bold p-2">Welcome to plantversity!</h1>
+    <p>
+    A gamified Progressive Web App (PWA) that helps users track the diversity of plant-based foods they consume and
+    motivates them to reach the goal of 30 different plants per week through progress tracking, badges, and educational
+    insights.
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
