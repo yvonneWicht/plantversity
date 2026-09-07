e459ebfd841f4c49530fbced82f0dca296f63c55
Yvonne Wicht <yw@krusemedien.com>
2026-09-07 13:51:57 +0200

    feature/ip-38: enhance meal management UI with conditional rendering for empty states and new ElementMeal component

---
+++ app/components/element/meal.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+  <div class="relative z-10 bg-white p-2 rounded-full inline-block w-full px-5">
+    {{ name }}
+
+    <ButtonPrimary
+      type="button"
+      class="absolute right-0 top-1/2 -translate-y-1/2 h-full aspect-square"
+      @click=""
+    >
+      >
+    </ButtonPrimary>
+  </div>
+</template>
